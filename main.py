e6f3fbeae853540045a0d3973a9de743214aaba4
luiz <luizgustavofcsg@hotmail.com>
2022-08-08 22:16:45 -0300

    training implementation

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from turtle import color
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import numpy as np
+
+from perceptron import Perceptron
 
 LEARNING_RATE = 0.1
 EPOCHS = 25
@@ -12,4 +13,20 @@ c[data[:, 2] == 0] = 'red'
 c[data[:, 2] == 1] = 'blue'
 print(c)
 plt.scatter(data[:, 0], data[:, 1], color=c)
-plt.show()
+#plt.show()
+
+perceptron = Perceptron(
+    LEARNING_RATE, 
+    data.shape[1] - 1, 
+    EPOCHS
+)
+
+print()
+
+training_set = list(zip(data[:,np.array([True, True, False])], data[:, 2]))
+
+print()
+
+log = perceptron.fit(training_set)
+for i in log:
+    print(i)
--- perceptron.py
+++ perceptron.py
@@ -3,7 +3,7 @@ import numpy as np
 from random import seed, random
 
 class Perceptron:
-    def __init__(self, learning_rate, number_of_inputs, seed = None):
+    def __init__(self, learning_rate, number_of_inputs, max_epochs, seed = None):
         self.learning_rate = learning_rate
         
         if(seed != None):
@@ -12,6 +12,63 @@ class Perceptron:
         
         self.weights = np.random.rand(number_of_inputs)
         self.bias = random()
+        print(f"generated initial calibration {self.weights}; {self.bias}")
+        self.max_epochs = max_epochs
 
+    def eval(self, inputs):
+        return np.sum(inputs * self.weights) + self.bias
 
-    
+    def g(self, r):
+        if(r >= 0): return 0
+        else: return 1
+
+    def step(self, inputs):
+        return self.g(self.eval(inputs))
+
+    def fit(self, training_set):
+        print(f'max_epochs={self.max_epochs}')
+
+        training_log = []
+
+        for i in range(self.max_epochs):
+            print(f"epoch {i}")
+
+            is_fitted = True
+            
+            for X, y in training_set:
+                #print(f"training item {X} -> {y}")
+                y_res = self.step(X)
+                #print(f"evaluation {y_res}")
+
+                training_log.append([i, X, y, y_res, self.weights, self.bias])
+
+                # prediction = 1; objective 0
+                if y_res > y: 
+                    self.weights += (self.learning_rate * X)
+                    self.bias += self.bias + self.learning_rate
+                    #print(f"new model calibration {self.weights}; {self.bias}")
+                    is_fitted = False
+                    continue
+
+                # prediction = 0; objective 1
+                if y_res < y: 
+                    self.weights -= (self.learning_rate * X)
+                    self.bias -= self.bias + self.learning_rate
+                    #print(f"new model calibration {self.weights}; {self.bias}")
+                    is_fitted = False
+                    continue
+            
+            if is_fitted == True:
+                break
+
+        return training_log
+
+    def test(self, test_params, test_results):
+        accurate_responses = 0
+
+        for X, y in zip(test_params, test_results):
+            y_pred = self.step(X)
+            if y_pred == y:
+                accurate_responses += 1
+
+            print(f"imputs {X} : predicted={y_pred}, actual={y}")
